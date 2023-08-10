9b472b55dc066aaf8d6acb693f59ebc24c6c8e16
Ali Sher <alisherwns3@gmail.com>
2023-08-10 21:55:30 +0500

    just started

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="app">
+          <div className="coming-soon">
+            <h1>Let the Magic Happen</h1>
+            <p>Get Ready to be Amazed</p>
+          </div>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
